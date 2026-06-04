e19b0baa7a8dff111d62a32c648988ede2618390
teacher <teacher@9PC0>
2026-06-04 19:56:08 +0300

    Добавьте файлы проекта.

---
+++ BV411/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BV411.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
