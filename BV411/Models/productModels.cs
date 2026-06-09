701ca2b4e4250801ce66de0bbbc6ed3be3ecb4ef
student <student@9PC3>
2026-06-09 21:43:33 +0300

    занятие 3

---
+++ BV411/Models/productModels.cs
@@ -0,0 +1,22 @@
+﻿namespace BV411.Models
+{
+    public class productModels
+    {
+        public int ip;
+        public string name;
+        public string description;
+        public int prise;
+
+        public productModels(int ip, string name, string description, int prise)
+        {
+            this.ip = ip;
+            this.name = name;
+            this.description = description;
+            this.prise = prise;
+        }
+        public override string ToString()
+        {
+            return $"{name}, {description}, {prise}\n";
+        }
+    }
+}
---
+++ BV411/Models/productRipositorie.cs
@@ -0,0 +1,12 @@
+﻿namespace BV411.Models
+{
+    public static class productRipositorie
+    {
+        public static List<productModels> products = new List<productModels>()
+        {
+            new productModels(0, "дети", "просто дети, хорошие работники", 120),
+            new productModels(1, "прах матери габена", "чуть солёный и сладний на вкус",300),
+            new productModels(2, "крек", "хыхыхыхыхыхы", 666)
+        };
+    }
+}
