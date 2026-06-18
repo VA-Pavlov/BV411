67c35a2dab2fc5e87ff2859545e2ce874214f2e6
teacher <teacher@9PC0>
2026-06-18 21:20:23 +0300

    Добавил страничку с карзиной товара

--- BV411/Models/Danik.cs
+++ BV411/Models/Danik.cs
@@ -3,8 +3,10 @@
     public class Danik
     {
         public int Id { get; set; }
+        public string Image { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public double Prise { get; set; }
         public string Linq { get; set; } = "https://localhost:7213/danials/info/";
     }
 }
--- BV411/Models/DanyPepos.cs
+++ BV411/Models/DanyPepos.cs
@@ -4,9 +4,10 @@
     {
         public static List<Danik> DanikList { get; set; } = new List<Danik>()
         {
-            new Danik(){Id = 1,Name = "Danik", Description="Не Даниил!" },
-            new Danik(){Id = 2,Name = "Даниил", Description="Не Дэни!" },
-            new Danik(){Id = 3,Name = "Маша", Description="Не Даниил и не Даник!" }
+            new Danik(){Id = 1,Name = "Danik", Description="Не Даниил!", Prise = 6000},
+            new Danik(){Id = 2,Name = "Даниил", Description="Не Дэни!", Prise = 10},
+            new Danik(){Id = 2,Name = "Vania", Description="Не Дэни!", Prise = 10.5},
+            new Danik(){Id = 3,Name = "Маша", Description="Не Даниил и не Даник!", Prise=100000 }
         };
     }
 }
